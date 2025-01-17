83599a94919d3441362908126067e58297aa0ffb
jansen <jansen.alvesr@gmail.com>
2025-01-17 20:26:23 -0300

    Fix: Corrigido erro ortográfico

--- poo-sabor-express/entities/menu/dish.py
+++ poo-sabor-express/entities/menu/dish.py
@@ -1,12 +1,12 @@
 from entities.menu.menu_item import MenuItem
 
 class Dish(MenuItem): # -------> Herança
-    def __init__(self, name, price, discription):
+    def __init__(self, name, price, description):
         super().__init__(name, price)
-        self._discription = discription
+        self._description = description
 
     def __str__(self):
-        return super().__str__() + f' | {self._discription}'
+        return super().__str__() + f' | {self._description}'
     
     def applyDiscount(self): #----> Polimorfismo
             self._price -= (self._price * 0.08) # Desconto de 8%
